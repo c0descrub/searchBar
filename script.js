01b5fba7d143c1e7f97394649dec76688c6ababd
Jordan <jordanjames1095@googlemail.com>
2020-10-26 11:51:32 +0000

    added some basic styles and made a start on the JS

---
+++ script.js
@@ -0,0 +1,3 @@
+var targetInput = document.getElementById ("country"),
+    results = document.getElementById("autocomplete-results"),
+    countryList = ['Albania', 'Colombia', 'Cuba', 'El Salvador', 'Jordan', 'Kenya', 'Nepal', 'Romania', 'Sri Lanka', 'Wales']
